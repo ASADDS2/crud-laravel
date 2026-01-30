deae715e62ba8388244dd9471a7168e0fe232eaa
Sebastian Arnache Cantillo <sarnachecantillo@gmail.com>
2026-01-30 11:38:51 -0500

    fix: Functionalities were corrected

--- crudproducts/app/Http/Controllers/ProductController.php
+++ crudproducts/app/Http/Controllers/ProductController.php
@@ -11,7 +11,7 @@ class ProductController extends Controller
     public function index()
     {
         // Enviamos todos los productos a la vista
-        return Inertia::render('Products/Index', [
+        return Inertia::render('products/products', [
             'products' => Product::latest()->get()
         ]);
     }
