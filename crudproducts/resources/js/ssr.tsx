2e62642ad57e41fb978236c16678bcf63cc8e57c
Sebastian Arnache Cantillo <sarnachecantillo@gmail.com>
2026-02-05 15:09:24 -0500

    feat: Laravel breeze was implemented

--- crudproducts/resources/js/ssr.tsx
+++ crudproducts/resources/js/ssr.tsx
@@ -1,16 +1,36 @@
 import { createInertiaApp } from '@inertiajs/react';
 import createServer from '@inertiajs/react/server';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { renderToString } from 'react-dom/server';
+import ReactDOMServer from 'react-dom/server';
+import { RouteName } from 'ziggy-js';
+import { route } from '../../vendor/tightenco/ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-createServer((page) =>
+import { Page } from '@inertiajs/core';
+import { PageProps } from './types/index';
+
+createServer((page: Page<PageProps>) =>
     createInertiaApp({
         page,
-        render: renderToString,
-        title: (title) => (title ? `${title} - ${appName}` : appName),
-        resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
-        setup: ({ App, props }) => <App {...props} />,
+        render: ReactDOMServer.renderToString,
+        title: (title) => `${title} - ${appName}`,
+        resolve: (name) =>
+            resolvePageComponent(
+                `./Pages/${name}.tsx`,
+                import.meta.glob('./Pages/**/*.tsx'),
+            ),
+        setup: ({ App, props }) => {
+            /* eslint-disable */
+            // @ts-expect-error
+            global.route<RouteName> = (name, params, absolute) =>
+                route(name, params as any, absolute, {
+                    ...page.props.ziggy,
+                    location: new URL(page.props.ziggy.location),
+                });
+            /* eslint-enable */
+
+            return <App {...props} />;
+        },
     }),
 );
--- crudproducts/resources/js/types/index.ts
+++ crudproducts/resources/js/types/index.ts
@@ -1,6 +1,21 @@
-export type * from './auth';
-
+import type { Config } from 'ziggy-js';
 import type { Auth } from './auth';
+
+export type User = {
+    id: number;
+    name: string;
+    email: string;
+    email_verified_at?: string;
+};
+
+export type PageProps<
+    T extends Record<string, unknown> = Record<string, unknown>,
+> = T & {
+    auth: {
+        user: User;
+    };
+    ziggy: Config & { location: string };
+};
 
 export type SharedData = {
     name: string;
